6de98b52ee4fc6625eefac118aef6c9d0ac8222f
kaua vitorio <kauavitorioof@gmail.com>
2020-12-09 18:54:53 -0300

    New SejaParceiroActivity

---
+++ app/src/main/java/com/example/coffeeforcodeapp/SejaParceiroActivity.java
@@ -0,0 +1,14 @@
+package com.example.coffeeforcodeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SejaParceiroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seja_parceiro);
+    }
+}
