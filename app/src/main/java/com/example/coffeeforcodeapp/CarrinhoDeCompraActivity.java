aaa59a129915b38d6b218289e37f5c7e48b98665
Yuri <yuridantaass@gmail.com>
2020-12-11 17:32:33 -0300

    i created a data processing

--- app/src/main/java/com/example/coffeeforcodeapp/CarrinhoDeCompraActivity.java
+++ app/src/main/java/com/example/coffeeforcodeapp/CarrinhoDeCompraActivity.java
@@ -2,13 +2,31 @@ package com.example.coffeeforcodeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class CarrinhoDeCompraActivity extends AppCompatActivity {
+    String emaillogado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrinho_de_compra);
+
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        emaillogado = bundle.getString("emailuser");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent voltaraoprincipal = new Intent(CarrinhoDeCompraActivity.this,PrincipalActivity.class);
+        voltaraoprincipal.putExtra("emailuser",emaillogado);
+        voltaraoprincipal.putExtra("statusavisoend","desativado");
+        startActivity(voltaraoprincipal);
+        finish();
+
     }
 }
