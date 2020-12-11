4953f417c7ff28e562390271e4ca3987d5b5c102
yuri <yuridantaass@gmail.com>
2020-12-11 17:46:10 -0300

    comment

--- app/src/main/java/com/example/coffeeforcodeapp/CadastrarenderecoActivity.java
+++ app/src/main/java/com/example/coffeeforcodeapp/CadastrarenderecoActivity.java
@@ -10,5 +10,11 @@ public class CadastrarenderecoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrarendereco);
+        /*
+
+
+
+        git commit -m
+         */
     }
 }
