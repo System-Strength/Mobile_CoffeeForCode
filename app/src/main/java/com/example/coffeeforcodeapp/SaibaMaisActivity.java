377400f0fd03c3cb589aef7f342e387d6d23ca6e
kaua vitorio <kauavitorioof@gmail.com>
2020-12-14 14:52:45 -0300

    Create SaibaMaisActivity

---
+++ app/src/main/java/com/example/coffeeforcodeapp/SaibaMaisActivity.java
@@ -0,0 +1,14 @@
+package com.example.coffeeforcodeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaibaMaisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saiba_mais);
+    }
+}
