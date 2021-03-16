22cb3bd355c8db293da5b9638872e45ef9a32de1
kaua vitorio <kauavitorioof@gmail.com>
2021-03-15 22:32:25 -0300

    refactored application for playstore

---
+++ app/src/main/java/co/ex/coffeeforcodeapp/SaibaMaisActivity.java
@@ -0,0 +1,14 @@
+package co.ex.coffeeforcodeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaibaMaisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saiba_mais);
+    }
+}
